32c00d1ecf23d71be8bc850b34d374c93ff584f4
lilianlacerda <lilianraiany@gmail.com>
2025-05-23 18:37:45 -0300

    primeiro commit

---
+++ src/main/java/com/liliSolution/filmListNew/listFilm/ListFilmApplication.java
@@ -0,0 +1,13 @@
+package com.liliSolution.filmListNew.listFilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liliSolution/filmListNew/listFilm/ListFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liliSolution.filmListNew.listFilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
