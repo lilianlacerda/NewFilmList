0a9b16a8e2bb261bee619b6319d9b038ef8f6542
lilianlacerda <lilianraiany@gmail.com>
2025-06-01 22:38:31 -0300

    add: teste de conexão com o banco de dados

--- src/test/java/com/liliSolution/filmListNew/listFilm/ListFilmApplicationTests.java
+++ src/test/java/com/liliSolution/filmListNew/listFilm/ListFilmApplicationTests.java
@@ -1,13 +1,23 @@
 package com.liliSolution.filmListNew.listFilm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class ListFilmApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private DataSource dataSource;
 
+	//Conexão com o banco de dados;
+	@Test
+	void testConnection() throws SQLException {
+		try (var connection = dataSource.getConnection()){
+			System.out.println("Conectou essa budega *-*");
+		}
+	}
 }
