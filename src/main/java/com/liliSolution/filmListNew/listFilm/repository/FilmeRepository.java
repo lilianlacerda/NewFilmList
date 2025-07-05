dfedcedd129cefe51750b8f8ce65a2229f25634e
lilianlacerda <lilianraiany@gmail.com>
2025-07-05 15:05:30 -0300

    add: repository do filme

---
+++ src/main/java/com/liliSolution/filmListNew/listFilm/repository/FilmeRepository.java
@@ -0,0 +1,9 @@
+package com.liliSolution.filmListNew.listFilm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.liliSolution.filmListNew.listFilm.entity.Filme;
+
+//Jpa repository serve para realizar operações de CRUD (Create, Read, Update, Delete) em entidades JPA
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
+};
