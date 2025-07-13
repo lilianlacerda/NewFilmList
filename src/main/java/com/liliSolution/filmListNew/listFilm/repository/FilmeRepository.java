675b451a1e0b36266f3af1e712838c6447e1b184
lilianlacerda <lilianraiany@gmail.com>
2025-07-13 19:27:34 -0300

    add: criar um método que verifica se o título do filme já existe

--- src/main/java/com/liliSolution/filmListNew/listFilm/repository/FilmeRepository.java
+++ src/main/java/com/liliSolution/filmListNew/listFilm/repository/FilmeRepository.java
@@ -6,4 +6,6 @@ import com.liliSolution.filmListNew.listFilm.entity.Filme;
 
 //Jpa repository serve para realizar operações de CRUD (Create, Read, Update, Delete) em entidades JPA
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
-};
+
+    boolean existsByTitulo(String titulo);
+}
