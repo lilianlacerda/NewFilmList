e6416c95077c6c4b985291e351c8677e636a4290
lilianlacerda <lilianraiany@gmail.com>
2025-07-16 22:43:26 -0300

    add: conversão de DTO para Entity e Entity para DTO

--- src/main/java/com/liliSolution/filmListNew/listFilm/service/FilmeService.java
+++ src/main/java/com/liliSolution/filmListNew/listFilm/service/FilmeService.java
@@ -2,11 +2,14 @@ package com.liliSolution.filmListNew.listFilm.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.liliSolution.filmListNew.listFilm.DTO.FilmeDTO;
 import com.liliSolution.filmListNew.listFilm.entity.Filme;
+import com.liliSolution.filmListNew.listFilm.exception.MoovieDeleteException;
 import com.liliSolution.filmListNew.listFilm.exception.MoovieNullException;
 import com.liliSolution.filmListNew.listFilm.exception.MoovieTituloException;
 import com.liliSolution.filmListNew.listFilm.repository.FilmeRepository;
@@ -20,7 +23,27 @@ public class FilmeService {
         this.filmeRepository = filmeRepository;
     }
 
-    public List<Filme> listarFilmes(){
+    public Filme converterDTOParaFilme (FilmeDTO filmeDTO){
+        Filme filme = new Filme();
+        filme.setTitulo(filmeDTO.getTitulo());
+        filme.setGenero(filmeDTO.getGenero());
+        filme.setDiretor(filmeDTO.getDiretor());
+        filme.setAnoLancamento(filmeDTO.getAnoLancamento());
+        filme.setSinopse(filmeDTO.getSinopse());
+        return filme;
+    }
+
+    public FilmeDTO converterFilmeParaDTO (Filme filme){
+        FilmeDTO filmeDTO = new FilmeDTO();
+        filmeDTO.setTitulo(filme.getTitulo());
+        filmeDTO.setGenero(filme.getGenero());
+        filmeDTO.setDiretor(filme.getDiretor());
+        filmeDTO.setAnoLancamento(filme.getAnoLancamento());
+        filmeDTO.setSinopse(filme.getSinopse());
+        return filmeDTO;
+    }
+
+    public List<FilmeDTO> listarFilmes(){
         Sort sort = Sort.by("titulo").ascending();
         return filmeRepository.findAll(sort);
     }
