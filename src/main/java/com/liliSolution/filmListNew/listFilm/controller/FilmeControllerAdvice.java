a1e4954c5a1fdff00602b41e9559f7615fef6040
lilianlacerda <lilianraiany@gmail.com>
2025-07-16 22:45:55 -0300

    Add: método de exception delete

--- src/main/java/com/liliSolution/filmListNew/listFilm/controller/FilmeControllerAdvice.java
+++ src/main/java/com/liliSolution/filmListNew/listFilm/controller/FilmeControllerAdvice.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.liliSolution.filmListNew.listFilm.exception.MoovieDeleteException;
 import com.liliSolution.filmListNew.listFilm.exception.MoovieNullException;
 import com.liliSolution.filmListNew.listFilm.exception.MoovieTituloException;
 
@@ -39,6 +40,13 @@ public class FilmeControllerAdvice extends ResponseEntityExceptionHandler{
         Map<String, Object> body = new HashMap<String, Object>();
         body.put("message", "Este filme já existe na sua lista. Acho que está na hora de assistir ele, hein?");
 
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    @ExceptionHandler(MoovieDeleteException.class)
+    public ResponseEntity<Object> captureErroDelete(){
+        Map<String, Object> body = new HashMap<String, Object>();
+        body.put("message", "Não foi possível deletar esse filme");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 }
